ab6b3ae57cbf799eea428b6b4f6e5e29a200fc43
Stephan Kroukamp <stephank@staycontactable.com>
2020-08-10 17:13:55 +0200

    initiate repository

---
+++ pages/books/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      {{ books }}
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  async asyncData ({ $axios }) {
+    const { data } = await $axios.get('books')
+
+    return { books: data }
+  }
+}
+</script>
